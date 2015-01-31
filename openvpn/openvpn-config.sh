ac00e94ab68c5366e04e2e9a904d659cdfe6feb9
Alfred <kanalfred@gmail.com>
2015-01-31 18:06:16 -0500

    seperate require-check from setup.sh

--- openvpn/openvpn-config.sh
+++ openvpn/openvpn-config.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 # Config & Setup OpenVpn.
--- openvpn/setup.sh
+++ openvpn/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# OpenVpn setup on Centos 7.x
+#
+# Initial Setup
 # 
 # arg0 execute command
 # arg1 first paremter
@@ -10,32 +11,8 @@ DIRNAME=`dirname $0`
 #############################
 #    Requirement Check
 #############################
-# Check Centos release file exist
-test ! -f /etc/centos-release && echo "OpenVpn script only support centos platform" && exit 1
+$DIRNAME/require-check.sh
 
-# Check Centos version - expect "CentOS Linux release 7.0.1406 (Core)"
-CENTOS_VERSION=`cat /etc/centos-release`
-if ! [[ $CENTOS_VERSION =~ ^CentOS[[:space:]]Linux[[:space:]]release[[:space:]]7.* ]]; then
-    echo 'OpenVpn script only support centos version 7, go away!' && exit 1
-fi
-
-# Check internet connection
-wget -q --tries=10 --timeout=20 --spider http://google.com
-# $? find error code of last execute command
-test ! -eq 0 && echo "No internet connection, go away!" && exit 1
-
-# Check requirment packages 
-test ! -f `which wget` && sudo yum -y install wget && echo "Installed missing wget package"
-test ! -f `which git` && sudo yum -y install git && echo "Installed missing git package"
-
-# Check EPEL repo
-EPEL=`yum repolist epel | grep epel`
-if [ -z "$EPEL" ]; then
-    # Install epel repo
-    # Todo: download epel from our server
-    #wget -O /tmp/epel-release-7-5.noarch.rpm http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-    sudo yum -y install $DIRNAME/packages/repo/epel-release-7-5.noarch.rpm
-fi
 
 #############################
 #    Setup Openvpn
